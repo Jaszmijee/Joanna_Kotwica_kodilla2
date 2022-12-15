ddc00586474a155d4ef25b1ec187407b304e8c18
Jaszmijee <jaszmije@gmail.com>
2022-12-15 11:32:43 +0100

    15.2 Added connections to database and connections between Controller and Mapper and Controller and DbService

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,19 +14,29 @@ import java.util.List;
 @RequestMapping("/v1/tasks")
 public class TaskController {
 
-    @GetMapping
-    public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @Autowired
+    public TaskController(DbService service, TaskMapper taskMapper) {
+        this.service = service;
+        this.taskMapper = taskMapper;
     }
 
     @GetMapping
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        Task task = service.getTaskById(taskId);
+        return (task != null) ? taskMapper.mapToTaskDto(task) : null;
+   }
 
     @DeleteMapping
     public void deleteTask(Long taskId) {
-
     }
 
     @PutMapping
@@ -32,6 +46,5 @@ public class TaskController {
 
     @PostMapping
     public void createTask(TaskDto taskDto) {
-
     }
 }
