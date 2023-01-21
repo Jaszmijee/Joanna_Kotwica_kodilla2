2446067441f36fa7cc321934a787535b7233ab21
Jaszmijee <jaszmije@gmail.com>
2023-01-21 11:30:01 +0100

    19.4 Added implementation of e-mails sending from application

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private final SimpleEmailService simpleEmailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        StringBuilder builder = new StringBuilder("Currently in database you got: " + size);
+        String message = ((size == 1) ? builder.append(" task") : builder.append(" tasks")).toString();
+        simpleEmailService.send(
+                Mail.builder()
+                        .mailTo(adminConfig.getAdminMail())
+                        .subject(SUBJECT)
+                        .message(message)
+                        .build()
+        );
+    }
+}
